7f762436409fae2f93e20a0cc02d534cb2077d7d
illumitata <gothic8282@gmail.com>
2018-12-10 17:27:08 +0100

    kNN and naiveBayes impl

--- src/nba.R
+++ src/nba.R
@@ -1,5 +1,9 @@
 # Uncomment and run to install packages used in project.
-# install.packages(c(""))
+# install.packages(c("plyr", "dplyr", "e1071", "class"))
+library(plyr)
+library(dplyr)
+library(e1071) # Naive Bayes
+library(class) # kNN
 
 # Loading data for project.
 # seasons_stats stands for csv file with stats of each player from each season.
@@ -25,7 +29,7 @@ players_stats_eu <- read.csv("./data/Players.csv", header = TRUE)
 #    wrong prediction of position. (Don't be Flash, don't mess the timelines)
 #
 # Now it's time to take some data out.
-picked_year = 1996
+picked_year = 2017
 seasons_stats <- seasons_stats[which(seasons_stats$Year >= 1980),]
 seasons_stats <- seasons_stats[which(seasons_stats$Year == picked_year),]
 
@@ -68,7 +72,6 @@ seasons_stats$PTSavg <- seasons_stats$PTS / seasons_stats$G
 
 seasons_stats.cleaned <- select(seasons_stats, 
                                 Player, 
-                                Pos,
                                 Height,
                                 Weight,
                                 PTSavg,
@@ -82,7 +85,8 @@ seasons_stats.cleaned <- select(seasons_stats,
                                 X3P.,
                                 X2P.,
                                 eFG.,
-                                FT.)
+                                FT.,
+                                Pos)
 
 # Now we have clean data from picked season. So.. what are we looking for? It's time to make classes!
 # We will be looking for players classified by Position (POS). Let's change column a little bit.
@@ -90,4 +94,102 @@ desired_position = "PG"
 seasons_stats.cleaned$Pos <- as.character(seasons_stats.cleaned$Pos)
 seasons_stats.cleaned$Pos[seasons_stats.cleaned$Pos != desired_position] <- "no"
 seasons_stats.cleaned$Pos[seasons_stats.cleaned$Pos == desired_position] <- "yes"
-# seasons_stats.cleaned$Pos <- as.factor(seasons_stats.cleaned$Pos)
+
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+
+# Set seed for all functions
+set.seed(picked_year)
+
+# Probabilities for each sample function
+picked_prob <- c(0.67, 0.33)
+
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+# Naive Bayes norm
+ind <- sample(2, nrow(seasons_stats.cleaned), replace = TRUE, prob = picked_prob)
+seasons_stats.training <- seasons_stats.cleaned[ind == 1, 1:ncol(seasons_stats.cleaned)]
+seasons_stats.test <- seasons_stats.cleaned[ind == 2, 1:ncol(seasons_stats.cleaned)]
+
+seasons_stats.training$Pos <- as.factor(seasons_stats.training$Pos)
+seasons_stats.test$Pos <- as.factor(seasons_stats.test$Pos)
+
+seasons_stats.model <-naiveBayes(Pos ~ 
+                                   Height + Weight + PTSavg + TRBavg + ASTavg + STLavg +
+                                   BLKavg + TOVavg + TS. + FG. + X3P. + X2P. + eFG. + FT.,
+                                 data = seasons_stats.training)
+# print(seasons_stats.model)
+
+# class(seasons_stats.test$Pos)
+# class(predict(seasons_stats.model, seasons_stats.test[, 1:(ncol(seasons_stats.test) - 1)]))
+
+pred_positions.bayes <- predict(seasons_stats.model, seasons_stats.test[, 2:(ncol(seasons_stats.test) - 1)])
+real_positions.bayes <- seasons_stats.test$Pos
+matrix_error.bayes <- table(pred_positions.bayes, real_positions.bayes)
+accuracy.bayes <- sum(diag(matrix_error.bayes)) / sum(matrix_error.bayes)
+
+# Who was picked wrong? Let's see...
+which_player.bayes <- seasons_stats.test
+which_player.bayes$Pred <- pred_positions.bayes
+# Add real positions to look inside problem.
+which_player.bayes$ExactPos <- seasons_stats$Pos[match(which_player.bayes$Player, seasons_stats$Player)]
+# Match the wrong results and summary them.
+which_player.bayes.results <- subset(which_player.bayes, Pos != Pred, select = colnames(which_player.bayes)) 
+which_player.bayes.positions_error <- count(which_player.bayes.results, which_player.bayes.results$ExactPos)
+
+accuracy.bayes
+matrix_error.bayes
+which_player.bayes.positions_error
+
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+
+fnorm <- function(x) {
+  return (x-min(x))/(max(x)-min(x))
+}
+
+seasons_stats.norm <- fnorm(seasons_stats.cleaned[, 2:(ncol(seasons_stats.cleaned) - 1)])
+seasons_stats.norm$Pos <- seasons_stats.cleaned$Pos
+seasons_stats.norm$Player <- seasons_stats.cleaned$Player
+
+ind <- sample(2, nrow(seasons_stats.norm), replace = TRUE, prob = picked_prob)
+seasons_stats.norm.training <- seasons_stats.norm[ind == 1, 1:ncol(seasons_stats.cleaned)]
+seasons_stats.norm.test <- seasons_stats.norm[ind == 2, 1:ncol(seasons_stats.cleaned)]
+
+knn.2 <- knn(seasons_stats.norm.training[, 1:(ncol(seasons_stats.norm.training) - 2)], 
+             seasons_stats.norm.test[, 1:(ncol(seasons_stats.norm.test) - 2)], 
+             cl = seasons_stats.norm.training$Pos, k = 2, prob = FALSE)
+
+pred_positions.knn <- knn.2
+real_positions.knn <- seasons_stats.norm.test$Pos
+matrix_error.knn <- table(pred_positions.knn, real_positions.knn)
+accuracy.knn <- sum(diag(matrix_error.knn)) / sum(matrix_error.knn)
+
+# Who was picked wrong? Let's see...
+which_player.knn <- seasons_stats.norm.test
+which_player.knn$Pred <- pred_positions.knn
+# Add real positions to look inside problem.
+which_player.knn$ExactPos <- seasons_stats$Pos[match(which_player.knn$Player, seasons_stats$Player)]
+# Match the wrong results and summary them.
+which_player.knn.results <- subset(which_player.knn, Pos != Pred, select = colnames(which_player.knn)) 
+which_player.knn.positions_error <- count(which_player.knn.results, which_player.knn.results$ExactPos)
+
+# message(sprintf("%f", accuracy))
+accuracy.knn
+matrix_error.knn
+which_player.knn.positions_error
+
+###################################################################
+###################################################################
+###################################################################
+###################################################################
+###################################################################
